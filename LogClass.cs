5b730b029e2e0b3eb488a027ae23c44b2af40c70
omer6167 <omerkucuk67@gmail.com>
2023-11-27 16:32:13 +0300

    27.11.2023

--- LogClass.cs
+++ LogClass.cs
@@ -7,19 +7,19 @@ namespace CSP.Util.Helper
 {
     public class LogClass
     {
-        public LogClass(int id, string message, string error = "undefined", string methodName = "", string project = "")
+        public LogClass(int id, string message, string error = "undefined", string method = "", string project = "")
         {
             Id = id;
             Message = message;
             Error = error;
-            MethodName = methodName;
+            Method = method;
             Project = project;
         }
 
         public int Id { get; set; }
         public string Message { get; set; }
         public string Error { get; set; }
-        public string MethodName { get; set; } = "";
+        public string Method { get; set; } = "";
         public string Project { get; set; } = "";
         public string Date { get; set; } = DateTime.Now.ToString("dd.MM.yyyy HH:mm");
 
