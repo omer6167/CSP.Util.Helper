112b89285b4a5aca735a5e351e46ac181b6ab144
omer6167 <omerkucuk67@gmail.com>
2023-12-07 04:11:09 +0300

    07.12.2023

--- Extension.cs
+++ Extension.cs
@@ -1,9 +1,9 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Text;
 
 namespace CSP.Util.Helper
 {
@@ -26,12 +26,38 @@ namespace CSP.Util.Helper
             return table;
         }
 
-        public static DataTable ToDataTable2<T>(this IList<T> list)
+        public static DataTable ToDataTableWithJson<T>(this IList<T> list)
         {
             string json = JsonConvert.SerializeObject(list);
             DataTable dt = JsonConvert.DeserializeObject<DataTable>(json);
 
             return dt;
+        }
+        public static bool IsJsonString(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) { return false; }
+            str = str.Trim();
+            if ((str.StartsWith("{") && str.EndsWith("}")) ||
+                (str.StartsWith("[") && str.EndsWith("]")))
+            {
+                try
+                {
+                    var obj = JToken.Parse(str);
+                    return true;
+                }
+                catch (JsonReaderException jex)
+                {
+                    return false;
+                }
+                catch (Exception ex) //some other exception
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
