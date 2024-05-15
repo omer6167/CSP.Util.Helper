9e30fb2d25ed029237bc3be152154eec078f42d3
Ömer KÜÇÜK <omerkucuk67@gmail.com>
2024-05-15 11:37:54 +0300

    15.05.2024

--- ServiceApiHelper.cs
+++ ServiceApiHelper.cs
@@ -111,7 +111,7 @@ namespace CSP.Util.Helper
             await process.SaveAndContinue();
         }
 
-        public static long StartFlow(Context context, string processName, Dictionary<string, object> values, string userID, string flowName = "Flow1", string AnAkisId = "", Event id = null)
+        public static long StartFlow(Context context, string processName, Dictionary<string, object> values, string userID, string flowName = "Flow1", string AnAkisId = "", Event? id = null)
         {
             id ??= new Event() { Id = 4 };
 
